dba7e031b46a7c7b0c3aabd700cb43c3fa909b2e
Kat Sullivan <kmsullivan012@gmail.com>
2017-06-15 17:49:36 -0400

    abstrated menu again, removed # from url

--- js/main.js
+++ js/main.js
@@ -1,36 +1,17 @@
 // Create an application module
 var app = angular.module('myApp', ["ngRoute"]);
 
-app.config(function($routeProvider) {
-  // $locationProvider.html5Mode(true);
-
+app.config(function($routeProvider, $locationProvider) {
+    $locationProvider.hashPrefix('');
     $routeProvider
-        .when('/portfolio', {
-            templateUrl: 'templates/portfolio.html'
-        })
-        .when('/events', {
-            templateUrl: 'templates/events.html'
-        })
-        .when('/cv', {
-            templateUrl: 'templates/cv.html'
-        })
         .when('/video', {
-            templateUrl: 'templates/video.html'
+            templateUrl: 'templates/video.html',
         })
-        // removed other routes ... *snip
-        // .otherwise({
-        //     redirectTo: '/home'
-        // }
-    // );
-        .otherwise({ redirectTo: '/' });
-
-    // enable html5Mode for pushstate ('#'-less URLs)
-    // $locationProvider.html5Mode(true);
-    // $locationProvider.hashPrefix('!');
-
+        .when('/portfolio', {
+            templateUrl: 'templates/portfolio.html',
+        })
+        .otherwise({
+            templateUrl: 'templates/home.html',
+        });
+    $locationProvider.html5Mode(true);
 });
-
-// // Initialize the application
-// app.run(['$location', function AppRun($location) {
-//     debugger; // -->> here i debug the $location object to see what angular see's as URL
-// }]);

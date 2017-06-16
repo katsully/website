729278cd8a6696573b2e9b23022220e174052537
Kat Sullivan <kmsullivan012@gmail.com>
2017-06-16 16:57:33 -0400

    added templates for other menu options, fixed styling for footer

--- js/main.js
+++ js/main.js
@@ -4,11 +4,20 @@ var app = angular.module('myApp', ["ngRoute"]);
 app.config(function($routeProvider, $locationProvider) {
     $locationProvider.hashPrefix('');
     $routeProvider
-        .when('/video', {
-            templateUrl: 'templates/video.html',
+        .when('/home', {
+            templateUrl: 'templates/home.html',
         })
         .when('/portfolio', {
             templateUrl: 'templates/portfolio.html',
+        })
+        .when('/events', {
+            templateUrl: 'templates/events.html',
+        })
+        .when('/cv', {
+            templateUrl: 'templates/cv.html',
+        })
+        .when('/video', {
+            templateUrl: 'templates/video.html',
         })
         .otherwise({
             templateUrl: 'templates/home.html',

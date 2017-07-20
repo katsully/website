2ed60ba5be39ddb894f4d367583bca724b599050
Kat Sullivan <kmsullivan012@gmail.com>
2017-07-19 20:27:16 -0400

    making progress on the project page, added in vimeo iframe

--- js/main.js
+++ js/main.js
@@ -35,37 +35,49 @@ app.config(function($routeProvider, $locationProvider) {
 
 app.factory('Works', function(){
 
-var Works = {}
+    var Works = {};
 
-Works.projects =[
-    {
-        id: 0,
-        name : "Aether",
-        string : "aether03",
-    },
-    {
-        id: 1,
-        name : "Torn",
-        string : "torn",
-    },
-    {
-        id: 2,
-        name : "Parsing Our Silent Language",
-        string : "parsingLang",
-    },
-    {
-        id: 3,
-        name : "Liable To Change",
-        string : "ltc",
+    Works.projects =[
+        {
+            id: 0,
+            name : "Aether",
+            string : "aether03",
+            description: "Aether is a live motion capture performance and installation designed and choreographed from the ancestral use of sacred geometry and the embodied elements: water, fire, earth and wind. The dancer's movement are tracked and projected in real-time, and the audience is given the opportunity to manipulate the visual elements on the dancers body through motion capture technology and physical computing.",
+            video: 'https://player.vimeo.com/video/179689753'
+        },
+        {
+            id: 1,
+            name : "Torn",
+            string : "torn",
+            video: ''
+        },
+        {
+            id: 2,
+            name : "Parsing Our Silent Language",
+            string : "parsingLang",
+            video: ''
+        },
+        {
+            id: 3,
+            name : "Liable To Change",
+            string : "ltc",
+            video: ''
+        }
+    ];  
+
+    Works.findByName = function(name) {
+        return Works.projects.filter(function(work) {
+            return work.name === name;
+        });  
     }
-];  
-return Works;
+    return Works;
 })
 
 app.controller("PortfolioController", function ($scope, Works) {
     $scope.works = Works;
 });
 
-app.controller("ProjectController", function ($scope, Works, $routeParams) {
-    $scope.work = Works.projects[$routeParams.workName];
+app.controller("ProjectController", function ($scope, Works, $routeParams, $sce) {
+    $scope.work = Works.findByName($routeParams.workName)[0];
+    $scope.url = $sce.trustAsResourceUrl($scope.work.video);
 });
